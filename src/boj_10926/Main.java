46dc8cf2067516a1dd08135a27078ce484b5bfdc
Jinwon-Dev <yjw9424@naver.com>
2023-02-11 22:53:46 +0900

    ??! #10926, 메모리: 14244KB, 시간: 128ms

---
+++ src/boj_10926/Main.java
@@ -0,0 +1,16 @@
+package boj_10926;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String id = br.readLine();
+        System.out.println(id + "??!");
+    }
+}
