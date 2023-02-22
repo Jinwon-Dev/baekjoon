2ac154140be888ae7e492471ccb487cc9a0dc221
Jinwon-Dev <yjw9424@naver.com>
2023-02-22 23:09:39 +0900

    개수 세기 #10807, 메모리: 14260KB, 시간: 128ms

---
+++ src/boj_10807/Main.java
@@ -0,0 +1,25 @@
+package boj_10807;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        int number = Integer.parseInt(br.readLine());
+        int result = 0;
+
+        for (int i = 0; i < N; i++) if (arr[i] == number) result++;
+        System.out.println(result);
+    }
+}
