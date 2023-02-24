3ad85a85152ea64c66eece7430c90f68dca71050
Jinwon-Dev <yjw9424@naver.com>
2023-02-24 20:30:48 +0900

    최댓값 #2562, 메모리: 14116KB, 시간: 124ms

---
+++ src/boj_2562/Main.java
@@ -0,0 +1,29 @@
+package boj_2562;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[9];
+        for (int i = 0; i < arr.length; i++) arr[i] = Integer.parseInt(br.readLine());
+
+        int max = 0;
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+                count = i;
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(count + 1);
+    }
+}
